1009b6cc11049e0857ba2f940e915578437e5dae
EuZerNyame <blair.daniel00@gmail.com>
2024-06-29 16:10:13 -0400

    Gave context to program

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,7 @@
+/*
+*CIS 1202 - Week 7 Source Code Management - 6/29/2024 - Daniel Blair 
+* This is a simple hello world program used to upload a program to understand source code management.
+*/
 #include <iostream>
 using namespace std;
 int main() {
